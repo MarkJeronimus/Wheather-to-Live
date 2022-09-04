b545697f8f1a73327b893bbab0ac382ac9114224
MarkJeronimus <mark.jeronimus@gmail.com>
2022-09-04 13:40:40 +0200

    Update text

--- src/org/digitalmodular/weathertolive/action/HelpAction.java
+++ src/org/digitalmodular/weathertolive/action/HelpAction.java
@@ -1,12 +1,10 @@
 package org.digitalmodular.weathertolive.action;
 
-import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 import org.jetbrains.annotations.Nullable;
@@ -31,35 +29,28 @@ public final class HelpAction extends AbstractAction {
 
 	@Override
 	public void actionPerformed(@Nullable ActionEvent e) {
-		JPanel p = new JPanel(new FlowLayout());
-
 		Frame frame = (Frame)parent.getTopLevelAncestor();
 
-		JLabel bigLabel = new JLabel("<html><center><h1>Weather to Live</h1><br>" +
-		                             "A pun on both<br>" +
-		                             "\"Where to live (for nice weather)\"<br>" +
-		                             "and \"Whether to live\" (hehe.)<br>" +
-		                             "<br>" +
-		                             "<b>It's a search engine</b> to find coordinates on the globe<br>" +
-		                             "that match your preferred weather parameters.<br>" +
-		                             "<br>" +
-		                             "<b>Usage:</b><br>" +
-		                             "The bottom panel shows individual weather<br>" +
-		                             "parameters, each with a min and max.<br>" +
-		                             "Changing the min and max changes the range of<br>" +
-		                             "that parameter that you consider pleasant.<br>" +
-		                             "The filters are combined to find the coordinates that match<br>" +
-		                             "<i>all</i> of the filters, and displayed in the main viewer.<br>" +
-		                             "Each month is filtered individually, unless the<br>" +
-		                             "'Filter entire year' checkbox is checked.<br>" +
-		                             "<br>" +
-		                             "The main viewer can be zoomed and dragged like a Maps app.<br>" +
-		                             "The colors are from the last touched parameter.");
+		JLabel bigLabel = new JLabel(
+				"<html><center><h1>Weather to Live</h1><br>" +
+				"A pun on both<br>" +
+				"\"Where to live (for nice weather)\" and \"Whether to live\" (hehe.)<br>" +
+				"<br>" +
+				"<b>It's a search engine</b> to find coordinates on the globe with your preferred weather.<br>" +
+				"<br>" +
+				"<b>Usage:</b><br>" +
+				"The bottom panel shows individual weather parameters, each with a min and max.<br>" +
+				"Changing the min and max changes the range of that climate element that you consider pleasant.<br>" +
+				"The filters are combined to find the coordinates that match <i>all</i> of the filters.<br>" +
+				"Coordinates that were filtered out are blacked out in the main viewer.<br>" +
+				"Each month is filtered individually, unless the 'Filter entire year' checkbox is checked.<br>" +
+				"<br>" +
+				"The main viewer can be zoomed and dragged like a Maps app.<br>" +
+				"The colors are from the last touched parameter.");
 		bigLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		p.add(bigLabel);
 
 		JOptionPane.showOptionDialog(frame,
-		                             p,
+		                             bigLabel,
 		                             frame.getTitle(),
 		                             JOptionPane.DEFAULT_OPTION,
 		                             JOptionPane.PLAIN_MESSAGE,
